dda9aa2229a9c301cdb3b73dda9e5a48f54966df
Antwa-sensei253 <ahmed.qwerty253@gmail.com>
2024-10-09 13:41:35 +0300

    init repo

---
+++ Books/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace WebBook.Repository.IRepository
+{
+    public interface IRepository
+    {
+        internal interface IRepository<T> where T : class
+        {
+             IEnumerable<T> GetAll();
+            T Get(Expression<Func<T,bool>> filter);
+            void Remove(T entity);
+            void Add(T entity);
+            void RemoveRange(IEnumerable<T> entity);
+             
+            
+        }
+    }
+}
